0d7563d3489bd32b1106e381cb43dc7a8b743571
ZamanLantra <zaman.lantra@warwick.ac.uk>
2023-03-06 15:58:11 +0000

    Add comment

--- fempic_new/cuda/cudakernels.cu
+++ fempic_new/cuda/cudakernels.cu
@@ -29,7 +29,10 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+//*********************************************
 // AUTO GENERATED CODE
+//*********************************************
+
 
 #include "../fempic.h"
 #include <oppic_cuda.h>
